1e026f87b9d1b4a2f99090732fbfa67ed8819bcc
changgohap <changgohap@gmail.com>
2023-12-07 17:40:22 +0900

    skills layout

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -3,12 +3,66 @@ import React from "react";
 export default function Skill() {
   return (
     <div id="skill">
-      <div className="skill_thumbnail">hello</div>
-      <div className="skill_sub_description">hello</div>
-      <div className="skill_main_description">hello</div>
-      <div className="skill_contents">
-        <div classname="skill_category"></div>
-        <div classname="skill_detail"></div>
+      <div className="thumbnail_box">
+        <div className="skill_thumbnail">
+          <img src="images/intro_logo.png" alt="intro_logo" />
+        </div>
+        <div className="skill_title">SKILLS</div>
+        <div className="skill_description">
+          My focused technologies: areas I have used and am currently studying
+        </div>
+      </div>
+      <div className="detail_box">
+        <div className="skill_contents">
+          <div className="skill_category">
+            <span>Core</span>
+          </div>
+          <div className="skill_box">
+            <div className="skill_name">
+              <span>HTML</span>
+            </div>
+            <div className="skill_name">
+              <span>CSS</span>
+            </div>
+            <div className="skill_name">
+              <span>JS</span>
+            </div>
+          </div>
+        </div>
+        <div className="skill_contents">
+          <div className="skill_category">
+            <span>Frameworks & Libraries</span>
+          </div>
+
+          <div className="skill_box">
+            <div className="skill_name">
+              <span>React</span>
+            </div>
+            <div className="skill_name">
+              <span>Redux</span>
+            </div>
+            <div className="skill_name">
+              <span>SCSS</span>
+            </div>
+          </div>
+        </div>
+        <div className="skill_contents">
+          <div className="skill_category">
+            <span>Tools & Platforms</span>
+          </div>
+
+          <div className="skill_box">
+            <div className="skill_name">
+              <span>Git</span>
+            </div>
+            <div className="skill_name">
+              <span>Figma</span>
+            </div>
+            <div className="skill_name">
+              <span>Vercel</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
